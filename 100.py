f5a835c1a10feb1c7880c3230b3278c791e24c18
yuorowi <129669044+yuorowi@users.noreply.github.com>
2023-12-07 19:05:34 +0800

    Update 100.py

--- 100.py
+++ 100.py
@@ -15,7 +15,7 @@ handler = WebhookHandler(CHANNEL_SECRET)
 OPENAI_API_KEY = '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'
 openai.api_key = OPENAI_API_KEY
 
-engine = "davinci"
+engine = "gpt-3.5-turbo-instruct"
 
 
 app = Flask(__name__)
