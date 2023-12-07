9193aad38cf7a22b793c274b5359f21f8441e6eb
sun <sunnyoursunshine@gmail.com>
2023-12-07 15:07:36 +0800

    Your commit message here

---
+++ 100.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import openai
+
+
+CHANNEL_SECRET = 'bd1f67e47488ef7d287541cfb175e6ec'
+CHANNEL_ACCESS_TOKEN = 'm/5ssSjjhD4saSEgKyIioep/OoJGzisdGHta3qxl2OhhJdvnmC+fnV4MCaJjavCB2BZBoRK6UYoAY8Y2D1L2iVizgzRwU3Q2QblOcdFlf5/H3XmMrZvSNwbYAB9SCJpHExP5tuhn5RpJDqsut4+imgdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+
+OPENAI_API_KEY = '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'
+openai.api_key = OPENAI_API_KEY
+
+
+
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_id = event.source.user_id
+    message_text = event.message.text
+
+
+    gpt_response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=message_text,
+        max_tokens=50
+    )
+    gpt_answer = gpt_response.choices[0].text.strip()
+
+  
+  
+
+    final_answer = f"answer: {gpt_answer}"
+
+   
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=final_answer)
+    )
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Dockerfile
@@ -0,0 +1,21 @@
+
+FROM python:3.8
+
+
+WORKDIR /app
+
+
+COPY . /app
+
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+
+EXPOSE 80
+
+
+ENV NAME World
+
+CMD ["python", "100.py"]
+
+
