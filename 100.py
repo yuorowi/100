56b1055583ba253453e52a2986277f3576a236d5
yuorowi <129669044+yuorowi@users.noreply.github.com>
2023-12-08 16:06:13 +0800

    Update 100.py

--- 100.py
+++ 100.py
@@ -12,38 +12,33 @@ handler = WebhookHandler(CHANNEL_SECRET)
 
 app = Flask(__name__)
 
-async def push_message_async(user_id, text):
-    try:
-        await line_bot_api.push_message(user_id, TextSendMessage(text=text))
-    except LineBotApiError as e:
-        print(f"Error pushing message to AI小幫手: {e}")
-
 @app.route("/callback", methods=['POST'])
-async def callback():
+def callback():
     signature = request.headers['X-Line-Signature']
-    body = await request.get_data(as_text=True)
-
+    body = request.get_data(as_text=True)
     try:
-        await handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
-async def handle_message(event):
-    user_message = event.message.text
+def handle_message(event):
+    user_id = event.source.user_id
+    message_text = event.message.text
 
-    if event.source.user_id == '@007omugu':
-        # 轉發使用者的訊息給 "AI小幫手"
-        asyncio.create_task(push_message_async('@AI小幫手的LINE ID', user_message))
+    try:
 
-        # 顯示 "AI小幫手" 回傳的訊息
-        reply_message = f"你對 AI小幫手 說了：{user_message}"
-        await line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_message)
-        )
+        final_answer = f"answer: {message_text}"
+
+
+    print(f"User ID: {user_id}, Message: {message_text}")
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=final_answer)
+    )
 
 if __name__ == "__main__":
     app.run(port=5000)
